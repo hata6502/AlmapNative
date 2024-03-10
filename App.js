524cd05f8aad2207aec7a0d2efa89f45539dfa29
hata6502 <7702653+hata6502@users.noreply.github.com>
2024-03-10 16:48:31 +0900

    Second commit

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as ImageManipulator from "expo-image-manipulator";
+import * as Location from "expo-location";
+import * as MediaLibrary from "expo-media-library";
+import { useEffect, useRef, useState } from "react";
+import { BackHandler } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
 
-export default function App() {
+export default App = () => {
+  const [location, setLocation] = useState();
+  const ref = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setLocation(null);
+        return;
+      }
+
+      setLocation(await Location.getCurrentPositionAsync());
+    })();
+  }, []);
+
+  const handleMessage = async (event) => {
+    const message = JSON.parse(event.nativeEvent.data);
+
+    switch (message.type) {
+      case "start": {
+        const { status } = await MediaLibrary.requestPermissionsAsync();
+        if (status !== "granted") {
+          BackHandler.exitApp();
+          return;
+        }
+
+        ref.current.postMessage(
+          JSON.stringify({ type: "progress", progress: 0 })
+        );
+
+        let pagedInfo;
+        let assetIndex = 0;
+        do {
+          pagedInfo = await MediaLibrary.getAssetsAsync({
+            after: pagedInfo?.endCursor,
+            createdAfter: message.after,
+            mediaType: "photo",
+            sortBy: [[MediaLibrary.SortBy.creationTime, true]],
+          });
+          console.log("cursor", pagedInfo.endCursor);
+
+          for (const asset of pagedInfo.assets) {
+            try {
+              ref.current.postMessage(
+                JSON.stringify({
+                  type: "progress",
+                  progress: (assetIndex + 1) / pagedInfo.totalCount,
+                })
+              );
+              assetIndex++;
+
+              const assetInfo = await MediaLibrary.getAssetInfoAsync(asset);
+              if (!assetInfo.location) {
+                continue;
+              }
+
+              const imageResult = await ImageManipulator.manipulateAsync(
+                assetInfo.localUri,
+                [
+                  {
+                    resize:
+                      assetInfo.height < assetInfo.width
+                        ? { width: 512 }
+                        : { height: 512 },
+                  },
+                ],
+                { base64: true }
+              );
+
+              ref.current.postMessage(
+                JSON.stringify({
+                  type: "importPhoto",
+                  id: assetInfo.id,
+                  dataURL: `data:image/jpeg;base64,${imageResult.base64}`,
+                  location: assetInfo.location,
+                  creationTime: assetInfo.creationTime,
+                })
+              );
+              console.log("ID", assetInfo.id);
+            } catch (exception) {
+              console.error(exception);
+            }
+          }
+        } while (pagedInfo.hasNextPage);
+
+        ref.current.postMessage(JSON.stringify({ type: "progress" }));
+        break;
+      }
+    }
+  };
+
+  if (location === undefined) {
+    return null;
+  }
+  const uri = `https://almap.hata6502.com/?${new URLSearchParams({
+    ...(location && {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    }),
+  })}`;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        ref={ref}
+        source={{ uri }}
+        originWhitelist={["https://almap.hata6502.com"]}
+        onMessage={handleMessage}
+      />
+    </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
