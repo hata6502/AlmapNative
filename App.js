f4c4f399bc0ccf74fb41ca1b12a3040d5dddb6ab
hata6502 <7702653+hata6502@users.noreply.github.com>
2024-03-16 18:31:54 +0900

    AppState

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import * as ImageManipulator from "expo-image-manipulator";
 import * as MediaLibrary from "expo-media-library";
 import { StatusBar } from "expo-status-bar";
-import { useRef } from "react";
-import { Alert, BackHandler } from "react-native";
+import { useEffect, useRef } from "react";
+import { Alert, AppState, BackHandler } from "react-native";
 import base64 from "react-native-base64";
 import { WebView } from "react-native-webview";
 
@@ -16,7 +16,100 @@ export default App = () => (
 );
 
 const Almap = () => {
+  const after = useRef(null);
+  const appState = useRef(AppState.currentState);
   const webView = useRef(null);
+
+  const importPhotos = async () => {
+    if (after.current === null) {
+      return;
+    }
+    const createdAfter = after.current;
+    after.current = null;
+
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status !== "granted") {
+      await new Promise((resolve) =>
+        Alert.alert(
+          "",
+          "アルバムへのアクセスが許可されていません。設定から許可してください。",
+          [{ onPress: resolve }]
+        )
+      );
+      BackHandler.exitApp();
+      return;
+    }
+
+    if (!createdAfter) {
+      await new Promise((resolve) =>
+        Alert.alert(
+          "",
+          "デバイス内のアルバムを取り込みます。しばらくお待ちください。",
+          [{ onPress: resolve }]
+        )
+      );
+    }
+
+    postMessageToWebView({ type: "progress", progress: 0 });
+
+    let assetIndex = 0;
+    let latestCreationTime = createdAfter;
+    let pagedInfo;
+    do {
+      pagedInfo = await MediaLibrary.getAssetsAsync({
+        after: pagedInfo?.endCursor,
+        createdAfter,
+        mediaType: "photo",
+        sortBy: [[MediaLibrary.SortBy.creationTime, true]],
+      });
+      console.log("cursor", pagedInfo.endCursor);
+
+      await Promise.all(
+        pagedInfo.assets.map(async (asset) => {
+          try {
+            const assetInfo = await MediaLibrary.getAssetInfoAsync(asset);
+            if (!assetInfo.location) {
+              return;
+            }
+
+            const imageResult = await ImageManipulator.manipulateAsync(
+              assetInfo.localUri,
+              [
+                {
+                  resize:
+                    assetInfo.height < assetInfo.width
+                      ? { width: 512 }
+                      : { height: 512 },
+                },
+              ],
+              { base64: true }
+            );
+
+            postMessageToWebView({
+              type: "importPhoto",
+              id: assetInfo.id,
+              dataURL: `data:image/jpeg;base64,${imageResult.base64}`,
+              location: assetInfo.location,
+              creationTime: assetInfo.creationTime,
+            });
+            latestCreationTime = assetInfo.creationTime;
+            console.log("ID", assetInfo.id);
+          } catch (exception) {
+            console.error(exception);
+          }
+        })
+      );
+
+      assetIndex += pagedInfo.assets.length;
+      postMessageToWebView({
+        type: "progress",
+        progress: assetIndex / pagedInfo.totalCount,
+      });
+    } while (pagedInfo.hasNextPage);
+
+    postMessageToWebView({ type: "progress" });
+    after.current = latestCreationTime;
+  };
 
   const postMessageToWebView = (message) => {
     webView.current.injectJavaScript(`
@@ -33,89 +126,32 @@ const Almap = () => {
 
     switch (message.type) {
       case "start": {
-        const { status } = await MediaLibrary.requestPermissionsAsync();
-        if (status !== "granted") {
-          await new Promise((resolve) =>
-            Alert.alert(
-              "",
-              "アルバムへのアクセスが許可されていません。設定から許可してください。",
-              [{ onPress: resolve }]
-            )
-          );
-          BackHandler.exitApp();
-          return;
-        }
-
-        if (!message.after) {
-          await new Promise((resolve) =>
-            Alert.alert(
-              "",
-              "デバイス内のアルバムを取り込みます。しばらくお待ちください。",
-              [{ onPress: resolve }]
-            )
-          );
-        }
-
-        postMessageToWebView({ type: "progress", progress: 0 });
-
-        let pagedInfo;
-        let assetIndex = 0;
-        do {
-          pagedInfo = await MediaLibrary.getAssetsAsync({
-            after: pagedInfo?.endCursor,
-            createdAfter: message.after,
-            mediaType: "photo",
-            sortBy: [[MediaLibrary.SortBy.creationTime, true]],
-          });
-          console.log("cursor", pagedInfo.endCursor);
-
-          await Promise.all(
-            pagedInfo.assets.map(async (asset) => {
-              try {
-                const assetInfo = await MediaLibrary.getAssetInfoAsync(asset);
-                if (!assetInfo.location) {
-                  return;
-                }
-
-                const imageResult = await ImageManipulator.manipulateAsync(
-                  assetInfo.localUri,
-                  [
-                    {
-                      resize:
-                        assetInfo.height < assetInfo.width
-                          ? { width: 512 }
-                          : { height: 512 },
-                    },
-                  ],
-                  { base64: true }
-                );
-
-                postMessageToWebView({
-                  type: "importPhoto",
-                  id: assetInfo.id,
-                  dataURL: `data:image/jpeg;base64,${imageResult.base64}`,
-                  location: assetInfo.location,
-                  creationTime: assetInfo.creationTime,
-                });
-                console.log("ID", assetInfo.id);
-              } catch (exception) {
-                console.error(exception);
-              }
-            })
-          );
-
-          assetIndex += pagedInfo.assets.length;
-          postMessageToWebView({
-            type: "progress",
-            progress: assetIndex / pagedInfo.totalCount,
-          });
-        } while (pagedInfo.hasNextPage);
-
-        postMessageToWebView({ type: "progress" });
+        after.current = message.after;
+        await importPhotos();
         break;
       }
     }
   };
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      "change",
+      async (nextAppState) => {
+        if (
+          appState.current.match(/inactive|background/) &&
+          nextAppState === "active"
+        ) {
+          await importPhotos();
+        }
+
+        appState.current = nextAppState;
+      }
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   return (
     <WebView
